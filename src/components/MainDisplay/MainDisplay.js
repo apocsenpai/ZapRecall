6426db7864756f34e1c7129fa6dd5a0b67c126d9
apocsenpai <jonatassennas@ufrrj.br>
2022-12-01 18:31:09 -0300

    feat(resultMessage): show a result message when all the questions are answered

--- src/components/MainDisplay/MainDisplay.js
+++ src/components/MainDisplay/MainDisplay.js
@@ -4,13 +4,15 @@ import QuestionList from "./QuestionList/QuestionList";
 import wrong from "../../assets/images/icone_erro.png";
 import almost from "../../assets/images/icone_quase.png";
 import right from "../../assets/images/icone_certo.png";
+import party from "../../assets/images/party.png";
+import sad from "../../assets/images/sad.png";
 import cards from "./cards";
 import { useState } from "react";
 
 const MainDisplay = ({ visibleWelcomeScreen }) => {
   const [answeredList, setAnsweredList] = useState([]);
   const [resultList, setResultList] = useState([]);
-  console.log(resultList)
+
   function showResultIcon(result) {
     if (result === "wrong") {
       return wrong;
@@ -19,6 +21,7 @@ const MainDisplay = ({ visibleWelcomeScreen }) => {
     }
     return right;
   }
+  console.log(resultList.length)
 
   function answeredQuestion(questionIndex, value) {
     setAnsweredList([...answeredList, questionIndex]);
@@ -38,14 +41,38 @@ const MainDisplay = ({ visibleWelcomeScreen }) => {
         showResultIcon={showResultIcon}
       />
       <footer>
+        {(resultList.length === cards.length) && (!resultList.includes("wrong") ? (
+          <ResultMessage
+            text={"Parabéns"}
+            icon={party}
+            message={"Você não esqueceu de nenhum flashcard!"}
+          />
+        ) : (
+          <ResultMessage
+            text={"Putz..."}
+            icon={sad}
+            message={"Ainda faltam alguns... Mas não desanime!"}
+          />
+        ))}
         <p>
           {answeredList.length}/{cards.length} CONCLUÍDOS
         </p>
         <div>
-        {!resultList.length ? "" : resultList.map((r, index)=><img key={index} src={showResultIcon(r)} />)}
+          {resultList.map((r, index) => (
+            <img key={index} src={showResultIcon(r)} />
+          ))}
         </div>
       </footer>
     </ScreenContainer>
+  );
+};
+
+const ResultMessage = ({text, icon, message}) => {
+  return (
+    <header>
+      <p><img src={icon}/> {text}</p>
+      <p>{message}</p>
+    </header>
   );
 };
 
--- src/components/MainDisplay/ScreenContainer.js
+++ src/components/MainDisplay/ScreenContainer.js
@@ -7,7 +7,7 @@ const ScreenContainer = styled.main`
   flex-direction: column;
   align-items: center;
   padding-bottom: 200px;
-  header {
+  &>header {
     display: flex;
     align-items: center;
     margin: 40px 0 20px 0;
@@ -40,6 +40,27 @@ const ScreenContainer = styled.main`
     font-size: 18px;
     color: #333333;
     padding: 10px;
+    header{
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      align-items: center;
+      width: 200px;
+      text-align: center;
+      p{
+        font-size: 18px;
+        font-family: 'Recursive';
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        img{
+          width: 23px;
+        }
+        &:nth-child(1){
+          font-weight: 700;
+        }
+      }
+    }
     div{
       display: flex;
       gap: 5px;
